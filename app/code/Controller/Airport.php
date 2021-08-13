206a8ac1bdd777799839ac7638be3a39da27c826
Evaldas1994 <evaldas.tuleikis@email.com>
2021-08-13 20:20:48 +0300

    Airport CRUD (index) done

---
+++ app/code/Controller/Airport.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Controller;
+
+use Core\Controller;
+
+class Airport
+{
+    public function index()
+    {
+        $controller = new Controller();
+        $controller->render("main/airport/index", []);
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
